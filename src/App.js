6306b8b1d138d6daff943924b45efcdd531a0b92
Mohib Akoum <2431135A@student.gla.ac.uk>
2021-09-17 17:34:45 +0100

    example start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import NavBar from "./components/NavBar";
+import Grid from "./components/Grid";
+import Footer from "./components/Footer";
+import "./App.css";
+
+import SecurityIcon from "@material-ui/icons/Security";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import TrendingUpIcon from "@material-ui/icons/TrendingUp";
+import ImportExportIcon from "@material-ui/icons/ImportExport";
+import ComputerIcon from "@material-ui/icons/Computer";
+import HttpIcon from "@material-ui/icons/Http";
+import { Typography } from "@material-ui/core";
+
+const theme = createTheme({
+   palette: {
+      primary: {
+         main: "#2e1667",
+      },
+      secondary: {
+         main: "#c7d8ed",
+      },
+   },
+   typography: {
+      fontFamily: ["Roboto"],
+      h4: {
+         fontWeight: 600,
+         fontSize: 28,
+         lineHeight: "2rem",
+      },
+      h5: {
+         fontWeight: 100,
+         lineHeight: "2rem",
+      },
+   },
+});
+
+const styles = makeStyles({
+   wrapper: {
+      width: "65%",
+      margin: "auto",
+      textAlign: "center",
+   },
+   bigSpace: {
+      marginTop: "5rem",
+   },
+   littleSpace: {
+      marginTop: "2.5rem",
+   },
+   grid: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexWrap: "wrap",
+   },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const classes = styles();
+
+   return (
+      <div className="App">
+         <ThemeProvider theme={theme}>
+            <NavBar />
+            <div className={classes.wrapper}>
+               <Typography variant="h4" className={classes.bigSpace} color="primary">
+                  At Rocket.io we are passionate about software
+               </Typography>
+               <Typography variant="h5" className={classes.littleSpace} color="primary">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales
+                  asidasodaosdkosa daskopd kopaskodp sakod koaskopd ksado pdas
+               </Typography>
+            </div>
+            <div className={`${classes.grid} ${classes.bigSpace}`}>
+               <Grid
+                  icon={
+                     <SecurityIcon
+                        style={{ fill: "#4360A6", height: "125", width: "125" }}
+                     />
+                  }
+                  title="Secure"
+                  btnTitle="Show me More"
+               />
+               <Grid
+                  icon={
+                     <EventNoteIcon
+                        style={{ fill: "#449A76", height: "125", width: "125" }}
+                     />
+                  }
+                  title="Reliable"
+                  btnTitle="Show me More"
+               />
+               <Grid
+                  icon={
+                     <TrendingUpIcon
+                        style={{ fill: "#D05B2D", height: "125", width: "125" }}
+                     />
+                  }
+                  title="Performant"
+                  btnTitle="Show me More"
+               />
+            </div>
+            <div className={`${classes.grid} ${classes.littleSpace}`}>
+               <Grid
+                  icon={
+                     <ImportExportIcon
+                        style={{ fill: "#4360A6", height: "125", width: "125" }}
+                     />
+                  }
+                  title="Modular"
+                  btnTitle="Show me More"
+               />
+               <Grid
+                  icon={
+                     <ComputerIcon
+                        style={{ fill: "#449A76", height: "125", width: "125" }}
+                     />
+                  }
+                  title="Multi-Platform"
+                  btnTitle="Show me More"
+               />
+               <Grid
+                  icon={
+                     <HttpIcon style={{ fill: "#D05B2D", height: "125", width: "125" }} />
+                  }
+                  title="Connected"
+                  btnTitle="Show me More"
+               />
+            </div>
+            <div className={classes.bigSpace}>
+               <Footer />
+            </div>
+         </ThemeProvider>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import YoutubeIcon from "@material-ui/icons/YouTube";
+
+function Footer() {
+   return (
+      <BottomNavigation>
+         <BottomNavigationAction
+            label="Facebook"
+            icon={<FacebookIcon style={{ fill: "#3b5998" }} />}
+         />
+         <BottomNavigationAction
+            label="Twitter"
+            icon={<TwitterIcon style={{ fill: "#1da1f2" }} />}
+         />
+         <BottomNavigationAction
+            label="Instagram"
+            icon={<InstagramIcon style={{ fill: "#c13584" }} />}
+         />
+         <BottomNavigationAction
+            label="Youtube"
+            icon={<YoutubeIcon style={{ fill: "#c4302b" }} />}
+         />
+      </BottomNavigation>
+   );
+}
+
+export default Footer;
